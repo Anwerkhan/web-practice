c5e098780cad2b0a43ece4aa91288d68fe82d30e
Anwer <anwer8313@gmail.com>
2022-01-08 13:43:05 +0500

    initial commit

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-$('.owl-carousel').owlCarousel({
-    rtl:true,
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
+<script>
+function kitchen()
+{
+    onclick='window.close'
+}
+</script>
